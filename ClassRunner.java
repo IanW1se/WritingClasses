87ecbb807f031cefbeab4d85beed1cb65c014cde
Collin Holmquist <collinholmquist@gmail.com>
2020-11-18 19:24:44 +0000

    Die Class fields and methods

--- ClassRunner.java
+++ ClassRunner.java
@@ -1,7 +1,20 @@
-public class ClassRunner.java {
+public class ClassRunner{
 
   public static void main(String [] args){
   
+    Dice d1 = new Dice(6);
+    Dice d2 = new Dice(6);
+
+    System.out.println("Pre Roll");
+    System.out.println(d1.getFaceValue());
+    System.out.println(d2.getFaceValue());
+
+    d1.roll();
+    d2.roll();
+
+    System.out.println("Post Roll");
+    System.out.println(d1.getFaceValue());
+    System.out.println(d2.getFaceValue());
       
   
   }
--- Dice.java
+++ Dice.java
@@ -1,6 +1,31 @@
+import java.util.Random;
+
 public class Dice {
 
-     
+     private int faceValue; 
+     private int numSides; 
+
+
+     //constructor
+     public Dice(int numSides){
+
+         this.numSides = numSides; 
+         faceValue = 1; 
+     }
+
+     public void roll(){
+
+         Random rand = new Random();
+         faceValue = rand.nextInt(numSides) + 1;
+
+     }
+
+    public int getFaceValue(){
+
+        return faceValue; 
+
+    }
+
 
 
 }
